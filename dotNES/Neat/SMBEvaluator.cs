6c40f9270dadfc92788e64964b13c2db83ed269e
Josh <joshua.a.beemster@gmail.com>
2018-03-25 11:42:22 +0200

    Started the eval function

--- dotNES/Neat/SMBEvaluator.cs
+++ dotNES/Neat/SMBEvaluator.cs
@@ -48,19 +48,40 @@ namespace dotNES.Neat
         }
 
         /// <summary>
-        /// Evaluate the provided IBlackBox against the random SuperMarioBros player and return its fitness score.
+        /// Evaluate the provided IBlackBox against the SMB Neat player and return its fitness score.
         /// </summary>
         public FitnessInfo Evaluate(IBlackBox box)
         {
             double fitness = 0;
 
+            // The amount of frames that can pass with Mario not making progress
+            int errorAllowance = 100;
+
             SMBNeatPlayer neatPlayer = new SMBNeatPlayer(box, ref _controller);
 
-            int[] inputs = _smbState.Inputs;
+            // Until Mario becomes stuck or dies feed each input frame into the neural network and make a move!
+            int levelX = _smbState.PlayerStats["x"];
 
-            neatPlayer.MakeMove(inputs);
+            while (errorAllowance > 0 || _smbState.GameStats["lives"] != 2)
+            {
+                neatPlayer.MakeMove(_smbState.Inputs);
 
-            // TODO: Based on move will need to extract fitness score
+                // Check whether Mario is advancing
+                if (levelX <= _smbState.PlayerStats["x"])
+                {
+                    errorAllowance--;
+                }
+                else
+                {
+                    levelX = _smbState.PlayerStats["x"];
+                }
+
+                // Update eval count
+                _evalCount++;
+
+                // Update fitness score
+                fitness++;
+            }
 
             // Return the fitness score
             return new FitnessInfo(fitness, fitness);
