c6960a3f2cd7d71564423e7f018fcbe47ae11ffa
Loic Nyssen <loic@nyssen.com.au>
2018-04-13 10:20:03 +0200

    Created PlayBest UI

---
+++ Testbed-UI/PlayBestUI.Designer.cs
@@ -0,0 +1,231 @@
+﻿namespace Testbed_UI
+{
+    partial class PlayBestUI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
+            this.UpButton = new System.Windows.Forms.PictureBox();
+            this.DownButton = new System.Windows.Forms.PictureBox();
+            this.LeftButton = new System.Windows.Forms.PictureBox();
+            this.RightButton = new System.Windows.Forms.PictureBox();
+            this.tableLayoutPanel4 = new System.Windows.Forms.TableLayoutPanel();
+            this.AButton = new System.Windows.Forms.PictureBox();
+            this.BButton = new System.Windows.Forms.PictureBox();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.tableLayoutPanel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.UpButton)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DownButton)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.LeftButton)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.RightButton)).BeginInit();
+            this.tableLayoutPanel4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.AButton)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BButton)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 0, 1);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 69.26677F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 30.73323F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(497, 641);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(491, 437);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 3;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 30F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 45F));
+            this.tableLayoutPanel2.Controls.Add(this.tableLayoutPanel3, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.tableLayoutPanel4, 2, 0);
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(0, 443);
+            this.tableLayoutPanel2.Margin = new System.Windows.Forms.Padding(0);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 1;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(494, 195);
+            this.tableLayoutPanel2.TabIndex = 2;
+            // 
+            // tableLayoutPanel3
+            // 
+            this.tableLayoutPanel3.ColumnCount = 5;
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel3.Controls.Add(this.UpButton, 2, 1);
+            this.tableLayoutPanel3.Controls.Add(this.DownButton, 2, 3);
+            this.tableLayoutPanel3.Controls.Add(this.LeftButton, 1, 2);
+            this.tableLayoutPanel3.Controls.Add(this.RightButton, 3, 2);
+            this.tableLayoutPanel3.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel3.Margin = new System.Windows.Forms.Padding(0);
+            this.tableLayoutPanel3.Name = "tableLayoutPanel3";
+            this.tableLayoutPanel3.RowCount = 5;
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(148, 195);
+            this.tableLayoutPanel3.TabIndex = 0;
+            // 
+            // UpButton
+            // 
+            this.UpButton.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.UpButton.Location = new System.Drawing.Point(61, 42);
+            this.UpButton.Name = "UpButton";
+            this.UpButton.Size = new System.Drawing.Size(23, 33);
+            this.UpButton.TabIndex = 0;
+            this.UpButton.TabStop = false;
+            // 
+            // DownButton
+            // 
+            this.DownButton.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.DownButton.Location = new System.Drawing.Point(61, 120);
+            this.DownButton.Name = "DownButton";
+            this.DownButton.Size = new System.Drawing.Size(23, 33);
+            this.DownButton.TabIndex = 1;
+            this.DownButton.TabStop = false;
+            // 
+            // LeftButton
+            // 
+            this.LeftButton.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.LeftButton.Location = new System.Drawing.Point(32, 81);
+            this.LeftButton.Name = "LeftButton";
+            this.LeftButton.Size = new System.Drawing.Size(23, 33);
+            this.LeftButton.TabIndex = 2;
+            this.LeftButton.TabStop = false;
+            // 
+            // RightButton
+            // 
+            this.RightButton.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.RightButton.Location = new System.Drawing.Point(90, 81);
+            this.RightButton.Name = "RightButton";
+            this.RightButton.Size = new System.Drawing.Size(23, 33);
+            this.RightButton.TabIndex = 3;
+            this.RightButton.TabStop = false;
+            // 
+            // tableLayoutPanel4
+            // 
+            this.tableLayoutPanel4.ColumnCount = 5;
+            this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel4.Controls.Add(this.AButton, 1, 1);
+            this.tableLayoutPanel4.Controls.Add(this.BButton, 3, 1);
+            this.tableLayoutPanel4.Location = new System.Drawing.Point(271, 0);
+            this.tableLayoutPanel4.Margin = new System.Windows.Forms.Padding(0);
+            this.tableLayoutPanel4.Name = "tableLayoutPanel4";
+            this.tableLayoutPanel4.RowCount = 3;
+            this.tableLayoutPanel4.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel4.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel4.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 30F));
+            this.tableLayoutPanel4.Size = new System.Drawing.Size(223, 195);
+            this.tableLayoutPanel4.TabIndex = 1;
+            // 
+            // AButton
+            // 
+            this.AButton.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.AButton.Location = new System.Drawing.Point(47, 100);
+            this.AButton.Name = "AButton";
+            this.AButton.Size = new System.Drawing.Size(38, 33);
+            this.AButton.TabIndex = 0;
+            this.AButton.TabStop = false;
+            // 
+            // BButton
+            // 
+            this.BButton.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.BButton.Location = new System.Drawing.Point(135, 100);
+            this.BButton.Name = "BButton";
+            this.BButton.Size = new System.Drawing.Size(38, 33);
+            this.BButton.TabIndex = 1;
+            this.BButton.TabStop = false;
+            // 
+            // PlayBestUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(496, 641);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "PlayBestUI";
+            this.Text = "Play Best";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.UpButton)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DownButton)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.LeftButton)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.RightButton)).EndInit();
+            this.tableLayoutPanel4.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.AButton)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BButton)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
+        private System.Windows.Forms.PictureBox UpButton;
+        private System.Windows.Forms.PictureBox DownButton;
+        private System.Windows.Forms.PictureBox LeftButton;
+        private System.Windows.Forms.PictureBox RightButton;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel4;
+        private System.Windows.Forms.PictureBox AButton;
+        private System.Windows.Forms.PictureBox BButton;
+    }
+}
